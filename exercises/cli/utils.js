77e7b6feadb86db600532eb2f007273d43f8b1a6
EddyVinck <eddyvinck95@gmail.com>
2020-03-02 22:04:17 +0100

    contact-book cli

--- exercises/cli/utils.js
+++ exercises/cli/utils.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
 // this path needs to be relative to work with fs
-const contactsLocation = 'contacts.json'
+const contactsLocation = path.join(__dirname, "contacts.json");
 
 /**
  * should read the contacts at the
@@ -10,21 +10,23 @@ const contactsLocation = 'contacts.json'
  * it to a js object
  */
 const getContacts = () => {
-  
-}
+  const contacts = fs.readFileSync(contactsLocation);
+  const parsed = JSON.parse(contacts);
+  return parsed;
+};
 
 /**
  * takes a contacts object, converts it to JSON
  * and saves it at the @contactsLocation path
  * @param {Object} contacts contacts object
  */
-const saveContacts = (contacts) => {
-
-}
+const saveContacts = contacts => {
+  const newContacts = JSON.stringify(contacts);
+  fs.writeFileSync(contactsLocation, newContacts, { encoding: "utf-8" });
+};
 
 module.exports = {
   contactsLocation,
   getContacts,
   saveContacts
-}
-
+};
