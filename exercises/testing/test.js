cbe90168ef7c485a387350b9e4940ca8862ecd09
EddyVinck <eddyvinck95@gmail.com>
2020-03-14 17:17:39 +0100

    api and testing exercises

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,18 @@
+const { fixId, findUser, deleteUser, users } = require("./users");
+
 // write some tests
-describe('users', () => {
-  
-})
+describe("users", () => {
+  test("fixId", () => {
+    expect(fixId("200")).toBe(200);
+  });
+  test("findUser", async () => {
+    const user = await findUser(5);
+    expect(user.id).toBe(5);
+  });
+  test("deleteUser", async () => {
+    // console.log(JSON.stringify(users, null, 2));
+    const originalUserCount = users.length;
+    await deleteUser(5);
+    expect(users.length).toBe(originalUserCount - 1);
+  });
+});
